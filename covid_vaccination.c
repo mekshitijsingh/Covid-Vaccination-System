da7342c2134c86b806d378f1143ff7d9b19a2501
Shaz'e <shazinabbas_m@srmap.edu.in>
2022-10-21 01:12:45 +0530

    Revert "Revert "Update covid_vaccination.c""

--- covid_vaccination.c
+++ covid_vaccination.c
@@ -189,6 +189,63 @@ if(p1.age>=18)
         if(p1.slot==3)
           printf("\nSlot: 4pm - 5pm");
     }
+      
+      
+      //sha
+      // Function To Take Candidate Numbers & Details
+void details()
+{
+	int i;
+	char a[20], b[6], c[40], d[20], e[20], f[3];
+
+	// Calling Heading() Function
+	heading();
+	printf(
+		"\t\t\t\tEnter Candidate Number (Max 4 People): ");
+	scanf("%d", &n);
+
+	// Taking Candidate Details
+	for (i = 1; i <= n; i++) {
+		// For Clear Screen
+		system("cls");
+
+		// Calling Heading() Function
+		heading();
+		printf("\t\t\t\tEnter The %dth Candidate Name: ",
+			i);
+		fflush(stdin);
+		gets(a);
+		printf("\t\t\t\tEnter The %dth Candidate Gender: ",
+			i);
+		fflush(stdin);
+		gets(b);
+		printf("\t\t\t\tEnter The %dth Candidate Id-Type: ",
+			i);
+		fflush(stdin);
+		gets(c);
+		printf(
+			"\t\t\t\tEnter The %dth Candidate Id-Number: ",
+			i);
+		fflush(stdin);
+		gets(d);
+		printf("\t\t\t\tEnter The %dth Candidate Mobile "
+			"Number: ",
+			i);
+		fflush(stdin);
+		gets(e);
+		printf("\t\t\t\tEnter The %dth Candidate "
+			"Co-Morbidity Status (Yes or No): ",
+			i);
+		fflush(stdin);
+		gets(f);
+
+		// Calling Function addnode()
+		addnode(a, b, c, d, e, f);
+	}
+}
+//sha
+      
+      
 
     if(p1.center==2)
     {
